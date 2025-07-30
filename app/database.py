ace782c38441ea4bbee478ba672f3f9ac8d0529a
Bissam Iftikhar <bissamiftikhar@gmail.com>
2025-07-31 03:59:20 +0500

    hah

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:yourpassword@localhost/password_manager"
+DATABASE_URL = "postgresql://postgres:bissam0913@localhost/password_manager"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
